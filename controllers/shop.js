7c0cabd7b83a9884c8406fc65a8f20f64ef99911
Tarok <T-arok@ukr.net>
2024-03-18 15:04:28 +0200

    Added pagination to the project.

--- controllers/shop.js
+++ controllers/shop.js
@@ -4,14 +4,33 @@ const PDFDocument = require('pdfkit');
 const Product = require('../models/product');
 const Order = require('../models/order');
 
+const ITEMS_PRE_PAGE = 2;
+
 exports.getProducts = (req, res, next) => {
-  Product.find()
+  const page = Number(req.query.page) || 1;
+  let totalItems;
+
+  Product.find().countDocuments()
+    .then((productNum) => {
+      totalItems = productNum;
+
+      return Product
+        .find()
+        .skip((page - 1) * ITEMS_PRE_PAGE)
+        .limit(ITEMS_PRE_PAGE);
+    })
     .then(products => {
       console.log(products);
       res.render('shop/product-list', {
         prods: products,
         pageTitle: 'All Products',
         path: '/products',
+        currentPage: page,
+        hasNextPage: ITEMS_PRE_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PRE_PAGE),
       });
     })
     .catch(err => {
@@ -39,12 +58,29 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.find()
+  const page = Number(req.query.page) || 1;
+  let totalItems;
+
+  Product.find().countDocuments()
+    .then((productNum) => {
+      totalItems = productNum;
+
+      return Product
+        .find()
+        .skip((page - 1) * ITEMS_PRE_PAGE)
+        .limit(ITEMS_PRE_PAGE);
+    })
     .then(products => {
       res.render('shop/index', {
         prods: products,
         pageTitle: 'Shop',
         path: '/',
+        currentPage: page,
+        hasNextPage: ITEMS_PRE_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PRE_PAGE),
       });
     })
     .catch(err => {
