cf29b88661de6188c0757535de9c0b3013e2e751
Tarok <T-arok@ukr.net>
2024-03-18 13:44:34 +0200

    Added checking if user has access to see invoices.

--- app.js
+++ app.js
@@ -94,6 +94,8 @@ app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
   // res.status(error.httpStatusCode).render('500');
+  console.log(error);
+
   res.status(500).render('500', {
     pageTitle: 'Error',
     path: '/500',
